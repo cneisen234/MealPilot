a8fa9b1319cb497d40adba236d5139d114984dab
Christopher Neisen <christopherjay71186@gmail.com>
2024-08-20 21:59:29 -0500

    adjusting for heroku

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static("build"));
 
 // Set up SendGrid
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -44,11 +44,6 @@ app.use("/api", miscRoutes);
 //   // Then schedule it to run daily
 //   setInterval(applyScheduledDowngrades, 24 * 60 * 60 * 1000);
 // };
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
 
 // Start server
 app.listen(port, () => {
