770f61f0247e5a7648906f662cac8c6a282b6380
Christopher Neisen <christopherjay71186@gmail.com>
2024-08-21 11:42:31 -0500

    adding middleware toroute to https

--- server/server.js
+++ server/server.js
@@ -45,6 +45,13 @@ app.use("/api", miscRoutes);
 //   setInterval(applyScheduledDowngrades, 24 * 60 * 60 * 1000);
 // };
 if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https") {
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    } else {
+      next();
+    }
+  });
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../build", "index.html"));
   });
