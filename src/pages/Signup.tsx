3ca7a0e7a4b33daf42d0bbfca15b35ae0a28c667
Christopher Neisen <christopherjay71186@gmail.com>
2024-09-08 21:15:34 -0500

    removed references to account creation

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -7,10 +7,8 @@ const Signup: React.FC = () => {
     <div className="center-container">
       <div className="content-wrapper">
         <div className="auth-form">
-          <h2>Create Your Account</h2>
           <SignupForm />
           <p style={{ marginTop: "20px", textAlign: "center" }}>
-            Already have an account?{" "}
             <Link to="/login" className="auth-link">
               Log in here
             </Link>
